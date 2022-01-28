ccbdf1f55e2f8d74ed6f6ae6dc8211d77793ce83
helloworld753315 <helloworld.cpp.h@gmail.com>
2022-01-29 01:49:58 +0900

    大幅改修

--- projects/detection.py
+++ projects/detection.py
@@ -1,8 +1,20 @@
-from email.mime import image
+# -*- coding: utf-8 -*-
+
 from importlib.resources import path
 import cv2
 import numpy as np
 
+import numpy as np
+from PIL import Image
+import time
+import csv
+import pprint
+
+import sys
+from pathlib import Path
+
+
+"""
 # cap = cv2.VideoCapture("./projects/images/vtest.avi")
 cap = cv2.VideoCapture(0)
 wait_secs = int(1000 / cap.get(cv2.CAP_PROP_FPS))
@@ -35,8 +47,7 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-print()
-
+"""
 
 """
 def detector(path="./projects/images/cap_02.jpg"):
@@ -63,4 +74,55 @@ def detector(path="./projects/images/cap_02.jpg"):
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imwrite('./projects/images/return.jpg', img)
+"""
+
+
+def drawRectangle(img, a, b, c, d):
+    sub_img = img[b:b + d, a:a + c]
+
+    edges = cv2.Canny(sub_img, lowThreshold, highThreshold)
+    pix = cv2.countNonZero(edges)
+
+    if pix in range(min, max):
+        cv2.rectangle(img, (a, b), (a + c, b + d), (0, 255, 0), 3)
+        spots.loc += 1
+    else:
+        cv2.rectangle(img, (a, b), (a + c, b + d), (0, 0, 255), 3)
+
+class spots:
+    loc = 0
+
+
+with open('./projects/csv/input.csv', 'r', newline='') as inf:
+    csvr = csv.reader(inf)
+    rois = list(csvr)
+
+rois = [[int(float(j)) for j in i] for i in rois]
+
+min = 1
+max = 2
+lowThreshold = 2
+highThreshold = 1
+
+path = "./projects/images/cap_01.jpg"
+
+img = cv2.imread(path)
+
+for i in range(len(rois)):
+    drawRectangle(img, rois[i][0], rois[i][1], rois[i][2], rois[i][3])
+
+
+"""
+
+image = cv2.imread("./projects/images/cap_01.jpg")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+edges = cv2.Canny(gray, 100, 1020)
+
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+count_objects_image = len(contours)
+
+print("画像内のオブジェクト数：", str(count_objects_image))
 """
