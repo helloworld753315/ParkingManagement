d9edddb6565ff07e32c37693e5a910ad1fcca860
helloworld753315 <helloworld.cpp.h@gmail.com>
2022-06-15 15:54:11 +0900

    svgの色を編集する箇所作成

---
+++ app/v2/src/components/ParkingInfo.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as CarIcon } from "../assets/svg/info.svg";
+
+const ParkingInfo = (props: any) => {
+ return (
+   <div>
+     <CarIcon></CarIcon>
+   </div>
+ );
+};
+
+export default ParkingInfo
