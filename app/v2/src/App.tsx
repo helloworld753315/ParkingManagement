2da1ca1e533a59e452ebb7411ed6d815fca449bc
helloworld753315 <helloworld.cpp.h@gmail.com>
2022-06-18 00:15:21 +0900

    コード整理

--- app/v2/src/App.tsx
+++ app/v2/src/App.tsx
@@ -3,21 +3,6 @@ import "./App.css";
 // import Info from './assets/svg/info.svg';
 import ParkingInfo from "./components/ParkingInfo";
 
-/*
-function return_color_id(rate: number) {
-  if (rate < 50) {
-    return 0
-  }
-  else if (50 <= rate && rate < 80) {
-    return 1
-  }
-  else if (rate >= 80) {
-    return 2
-  }
-}
-*/
-
-
 function App() {
   return (
     <div className="App">
