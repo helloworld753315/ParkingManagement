005b9430ea71e0a27ec17b014d6043628e40480b
helloworld753315 <helloworld.cpp.h@gmail.com>
2022-06-18 00:14:42 +0900

    混雑率表示用のコンポーネントを修正

--- app/v2/src/App.tsx
+++ app/v2/src/App.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./App.css";
 // import Info from './assets/svg/info.svg';
-import ParkingInfo from './components/ParkingInfo'
+import ParkingInfo from "./components/ParkingInfo";
+
+/*
+function return_color_id(rate: number) {
+  if (rate < 50) {
+    return 0
+  }
+  else if (50 <= rate && rate < 80) {
+    return 1
+  }
+  else if (rate >= 80) {
+    return 2
+  }
+}
+*/
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ParkingInfo/>
+      <h1 className="font-mono text-left text-3xl m-20">
+        駐車場情報見れるくん
+      </h1>
+      <header className="App-header">  
+        <ParkingInfo rate={0} />
       </header>
     </div>
   );
